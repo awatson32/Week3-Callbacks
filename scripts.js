feacc801b6762212f73e6835d2299b5b0d385a8b
awatson32 <audrey.watson32@gmail.com>
2016-03-09 14:52:21 -0600

    Not sure why I'm getting NaN error

--- scripts.js
+++ scripts.js
@@ -1,11 +1,14 @@
 $(document).ready(function() {
     
-    $('#addNum').on('click', function add(err, result) {
-        if (err) {
-            alert('Nope, sorry!');
-        } else {
-            alert('The answer is: ' + result);
-        }
+    $('#addNum').on('click', function add(op1, op2) {
+        davidsLib.add(function(err, result) {
+            if (err) {
+                alert(err);
+            } else {
+                console.log('The answer is: ' + result);
+            }
+            
+        })
     });
     
 })
